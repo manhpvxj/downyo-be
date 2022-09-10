2c4b37e1bab29359a3e05afacd62af9cf09c3294
bottle alimi <phammanhtb2001@gmail.com>
2022-09-10 16:23:54 +0700

    added verify token

--- src/middleware/verifyMiddleware.js
+++ src/middleware/verifyMiddleware.js
@@ -1,20 +1,26 @@
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
 const User = require("../models/userModel");
+const auth = require("./authMiddleware");
 
-const auth = asyncHandler(async (req, res, next) => {
-  const token = req.headers.authorization;
+const verify = asyncHandler( async (req, res, next) => {
+    const token = req.headers.authorization;
   console.log(token);
   if (token) {
     try {
       // Get token from header
       const accessToken = req.headers.authorization.split(" ")[1];
-      console.log(accessToken);
       // Verify token
       const decoded = jwt.verify(accessToken, process.env.JWT_ACCESS_KEY);
       // Get user from the token
       req.user = await User.findById(decoded.id).select("-password");
-      next();
+      if(req.user.username == req.params.id) {
+        console.log("passed");
+        next();
+      }
+      else {
+        res.status(400).json("You are not allowed to do this");
+      }
     } catch (error) {
       res.status(403);
       throw new Error("Not authorization");
@@ -24,6 +30,7 @@ const auth = asyncHandler(async (req, res, next) => {
     res.status(401);
     throw new Error("You need to login");
   }
-});
+})
 
-module.exports = { auth };
+
+module.exports = { verify };
--- src/routes/webRoute.js
+++ src/routes/webRoute.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const { registerUser, loginUser } = require("../controllers/authController");
 const { editProfile } = require("../controllers/editProfileController");
 const { getUser } = require("../controllers/userController");
-const { protect } = require("../middleware/authMiddleware");
-
+const { auth } = require("../middleware/authMiddleware");
+const { verify } = require("../middleware/verifyMiddleware");
 const initWebRoutes = (app) => {
     router.get("/", (req, res) => {
         res.send("Hello world from route~")
@@ -13,8 +13,8 @@ const initWebRoutes = (app) => {
     router.post("/auth/register", registerUser);
     router.post("/auth/login", loginUser);
     router.get("/users/:id", getUser);
-    router.post("/users/:id/edit", protect, editProfile);
-    router.get("/users/:id/edit", protect, getUser);
+    router.post("/users/:id/edit", auth, verify, editProfile);
+    router.get("/users/:id/edit", auth, verify, getUser);
     return app.use("/api", router);
 }
 
